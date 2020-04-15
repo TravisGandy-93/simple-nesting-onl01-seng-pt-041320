200bcf4725de6b8208cad3e6806bc59960d2bf6a
Travis Gandy <travis_gandy@yahoo.com>
2020-04-15 09:42:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+   dennis_language = programmer_hash[:dennis_ritchie][:languages]
+   dennis_language.to_s
 end
 
 def adding_matz
